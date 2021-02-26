d6b8ab6a2941446c246fc54267bb1c64b4d88b0c
BEnelson <70784791+NELSONEN98@users.noreply.github.com>
2021-02-26 14:59:08 -0500

    Update dashboard.php

--- src1/View/partials/dashboard.php
+++ src1/View/partials/dashboard.php
@@ -1,46 +1,84 @@
 <div id="page-wrapper">
             <div class="row">
                 <div class="col-lg-12">
-                    <CENTER><h1 class="page-header">Moda Internacional
-                    <h1 class="page-header">Sistema De Despachos Vida Plena </h2></CENTER>
+                    <CENTER><h1 class="page-header">Moda Internacional&nbsp;<img  width="50" height="50" src="../../web/dist/css/milogo.png">
+                    <h1 class="page-header">Sistema De Despachos Vida Plena </h2></CENTER> 
                 </div>
                 <!-- /.col-lg-12 --> 
-                 <div class="panel-body">
-                            
-                           <!-- <button class="btn btn-default btn-lg" data-toggle="modal" data-target="#myModal">
-                                Guía <i class="fa fa-file-text-o "></i>
-                            </button>
-                            <!-- Modal -->
-                            <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-                                <div class="modal-dialog">
-                                    <div class="modal-content">
-                                        <div class="modal-header">
-                                            <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                                          
-                                        </div>
-                                        <div class="modal-body">
-                                            
-                <div class="col-lg-12">
-                    <div class="panel panel-default">
-                       
-                        <!-- /.panel-heading -->
-                        <div class="panel-body">
-                            <!-- Nav tabs -->
-                            <ul class="nav nav-tabs">
-                                <li class="active"><a href="#home"  data-toggle="tab">VIDA PLENA DESPACHADA</a>
-                                </li>
-                                <li><a href="#profile" data-toggle="tab">VIDA PLENA POR CONFIRMAR</a>
-                                </li>
-                                
-                            </ul>
-
-                            <!-- Tab panes -->
-                           
                
-                        <!-- /.panel-body -->
+
+
+                <div class="col-lg-3 col-md-6">
+                    <div class="panel panel-info">
+                        <div class="panel-heading">
+                            <div class="row">
+                                <div class="col-xs-3">
+                                    <i class="fa fa-pencil-square fa-5x"></i> 
+                                </div>
+                                <div class="col-xs-9 text-right">
+                                    <div class="huge">*</div>
+                                    <div></div>
+                                </div>
+                            </div>
+                        </div>
+                        <a href="index.php?modulo=Vidaplena&controlador=vidaplena&funcion=listar">
+                            <div class="panel-footer">
+                                <span class="pull-left">VP Por Despachar</span>
+                                <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                                <div class="clearfix"></div>
+                            </div>
+                        </a>
                     </div>
-                    <!-- /.panel -->
-                </div>                                        </div>
+                </div>
+
+
+
+                   <div class="col-lg-3 col-md-6">
+                    <div class="panel panel-info">
+                        <div class="panel-heading">
+                            <div class="row">
+                                <div class="col-xs-3">
+                                    <i class="fa fa-shopping-cart fa-5x"></i> 
+                                </div>
+                                <div class="col-xs-9 text-right">
+                                    <div class="huge">*</div>
+                                    <div></div>
+                                </div>
+                            </div>
+                        </div>
+                        <a href="index.php?modulo=Vidaplena&controlador=vidaplena&funcion=listarIn">
+                            <div class="panel-footer">
+                                <span class="pull-left">VP Despachada</span>
+                                <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                                <div class="clearfix"></div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                   <div class="col-lg-3 col-md-6">
+                    <div class="panel panel-info">
+                        <div class="panel-heading">
+                            <div class="row">
+                                <div class="col-xs-3">
+                                    <i class="fa fa-list-alt fa-5x"></i> 
+                                </div>
+                                <div class="col-xs-9 text-right">
+                                    <div class="huge">*</div>
+                                    <div></div>
+                                </div>
+                            </div>
+                        </div>
+                        <a href="index.php?modulo=Vidaplena&controlador=vidaplena&funcion=listarinv">
+                            <div class="panel-footer">
+                                <span class="pull-left">Inventario</span>
+                                <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                                <div class="clearfix"></div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+
+         </div>
                                         
 
 
