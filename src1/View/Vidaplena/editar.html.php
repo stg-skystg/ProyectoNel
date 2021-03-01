926704c46374833b2293e980e2591f496fbdbaa2
BEnelson <70784791+NELSONEN98@users.noreply.github.com>
2021-03-01 12:30:20 -0500

    Update editar.html.php

--- src1/View/Vidaplena/editar.html.php
+++ src1/View/Vidaplena/editar.html.php
@@ -1,47 +1,73 @@
 <div id="page-wrapper">
             <div class="row">
                 <div class="col-lg-12">
-                    <h1 class="page-header">Comprobar Vida Plena&nbsp</h1>
+                    <h1 class="page-header">Comprobar Vida Plena&nbsp<i class="fa  fa-table fa-fw"></i></h1>
                 </div>
-                <!-- /.col-lg-12 -->
+               
             </div>
-            <!-- /.row -->
+            <!-- /.row -->         
             <div class="row">
                <div class="panel panel-default">
+                  
                         <div class="panel-heading">
-                            VP2021
+                             Vida Plena 2021
                         </div>
-                        <div class="panel-body">
-                            <div class="row">
-                                <div class="col-lg-12">
-                                    <form role="form" name="frm_fac" method="post" action="<?php echo getUrl("Vidaplena","vidaplena","postEditar"); ?>">
+
+                        <div class="panel-body">  
+                            
+                                   
                                         <div class="form-group">
 
-<?php foreach ($producto as $productos) {
-  echo $productos['9'];
-}
+                                          <table class="table" width="100px">
 
-                                          $productos['idpedido'];
+                                            <tr>
+                                              
+                                                  <th scope="col">ID VIDA PLENA</th>
+                                                  <th scope="col">NOMBRE GERENTE</th>
+                                                  <th scope="col">CEDULA GERENTE</th>
 
-                                          ?>
-                                            <label>ID VIDA PLENA:</label>
+                                                  
+                                            </tr>
+
+                                            </thead>
+                                              <tbody>
+                                                <tr>
+
+                                                  <td> <label><?php foreach($producto as $productos){} echo $productos[1]?></label>
+                                                  </td>
+
+                                                  <td> <label><?php foreach($producto as $productos){} echo $productos[4]?></label></td>
+
+                                                  <td>   <label>&nbsp;<?php foreach($producto as $productos){} echo $productos[3]?></label>
+                                                </td>
 
 
+                                                </tr>
 
+                                          </table>
 
-                                        <label><?php foreach ($producto as $productos){} echo $productos['idpedido']; ?></label>
+                                           
 
-                                        <input class="form-control" name="idpedido" type="hidden" value="<?php echo $productos['idpedido']; ?>">
+                                          <input type="hidden" name="Cod_Pedido" id="Cod_Pedido" value="<?php echo $productos['idpedido'];?>"></input>
 
-                                        <input class="form-control" name="cantsol" type="hidden" value="<?php echo $productos['cantsol']; ?>"> 
+                                          <div class="form-group input-group" >
+                                            <span class="input-group-addon" > <i class="fa  fa-barcode fa-fw"></i>  </span>
+                                            <input type="text" class="form-control"  width="100" placeholder="|||||||||||||||||||||||||||||||||||||||||||||">
+                                        </div>
+
                                         
-                                            
-                                       
-                                       
-                                     
-                                       <table class="table table-striped table-bordered" width="500px">
+                                <form role="form" name="idpedido" method="post" action="<?php echo getUrl("Vidaplena","vidaplena","postEditar"); ?>">
+
+
+
+                                   <div class="table-responsive">
+                                            <table class="table table-striped table-bordered table-hover" width="500px">
                                               <thead>
                                                 <tr>
+
+
+
+                                                  <th scope="col">*</th>
                                                   <th scope="col">Codigo</th>
                                                   <th scope="col">Producto</th>
                                                   <th scope="col">Color</th>
@@ -56,55 +82,98 @@
                                               </thead>
                                               <tbody>
                                                 <tr>
-                                                  <?php
-                                                      foreach ($producto as $productos){                                
-                                                   echo '<tr class="gradeA odd" role="row" style="text-align: center;">
-                                                <td>'.$productos['referenc'].'</td>
-                                                <td>'.$productos['nompro'].'</td>
-                                                <td>'.$productos['color'].'</td>
-                                                <td>'.$productos['talla'].'</td>
-                                                <td>'.$productos['ubica'].'</td>
-                                                <td>'.$productos['cantsol'].'</td>
-
-                                                <td> 
-                                       
-                                            <input class="form-control" type="number" style="width : 60px; heigth : 1px" name="cantvend" id="cantvend"  value="'.$productos['9'].'"  max="'.$productos['cantsol'].'" min="0">
+                                                <?php
+                                                $i =0;
+                                                foreach ($producto as $productos){
+                                                    //$i = $i + 1;
+                                                    echo '<tr class="gradeA odd" role="row" style="text-align: center;">
 
 
+                                                    <td> 
 
-                                                </td>
-                                             
-                                                <td>'.$productos['inv'].'</td>
-                                               
-                                         
-                                             
-                                             </tr>';
 
-                                    }
+                                                    <input type="hidden" class="form-control" type="number" style="width : 60px; heigth : 1px" name="select[]" id="select[]" value="'.$productos['contador'].'">
+
+
+                                                    </td>
+
+
+
+                                                    <td>'.$productos['referenc'].'</td>
+                                                    <td>'.$productos['nompro'].'</td>
+                                                    <td>'.$productos['color'].'</td>
+                                                    <td>'.$productos['talla'].'</td>
+                                                    <td>'.$productos['ubica'].'</td>
+                                                    <td>'.$productos['cantsol'].'</td>
+
+                                                    <td> 
+
+                                                    <input class="form-control" type="number" style="width : 60px; heigth : 1px" name="selectinput[]" id="selectinput[]" value=""  max="'.$productos['cantsol'].'" min="0">
+
+                                                    </td>
+
+                                                    <td>'.$productos['inv'].'</td>
+
+                                                    </tr>';
+
+                                                }
                                 ?>
                                     
 
                                             
- 
+
                                                 
                               </tbody>
                         </table>
+
+                      </div>
+
                                        
-                      </div>                  
-                                       
-                                       
-                                        </div></div>
-                                        
-                                        
-                                        <button type="submit" class="btn btn-success">Enviar</button>
-                                        <button class="btn btn-danger">Cancelar<a href="<?php echo 
-                                        getUrl("inventario","inventario","listar"); ?>"></a></button>
-                                    </form>
+                  </div>
+                 
+                       
+                <?php
+              
+                    echo ' <a href="index.php?modulo=Vidaplena&controlador=vidaplena&funcion=postEditar&idpedido='.$productos['idpedido'].'"><button class="btn btn-success"><i class="fa fa fa-check">&nbsp;Despachar</i></button></a>        
+                    </td>';
+ 
+                ?>
+                               
+                   
+
+
+                     
+                   </form>&nbsp; &nbsp; <?php    
+                    
+            
+                     echo ' <a href="index.php?modulo=Vidaplena&controlador=vidaplena&funcion=listar"><button class="btn btn-danger"><i class="fa  fa-times">&nbsp;&nbspCancelar&nbsp&nbsp</i></button></a>
+                    </td>';
+              ?>
+                      
+
+                
+              </form>  
+
+
+
+
+
+
+
+
+                
+
+             
+
+                
+
+
+
+             
                                 </div>
                             </div>
                             <!-- /.row (nested) -->
                         </div>
                         <!-- /.panel-body -->
                     </div> 
-            </div>
-</div>
+     
